38afe6210a7f6f4a2ac9f9f91841a8180d34533b
Gabriel Leopoldo Barrau <leopoldo.barrau@lepis.com.ar>
2020-07-01 16:56:53 -0300

    inspector context menu

--- src/components/Inspector.tsx
+++ src/components/Inspector.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import {HTMLSelect, ITreeNode, Tree} from "@blueprintjs/core";
+import {ContextMenu, HTMLSelect, ITreeNode, Menu, Tree} from "@blueprintjs/core";
 import {Project} from "@wayred/core";
 import {ApplicationState, Workspace} from "../types";
 import {Dispatch} from 'redux';
 import {connect} from "react-redux";
 import {viewSelect} from "../actions/view.actions";
+import {jsx} from "@emotion/core";
 
 type InspectorProps = {
   workspace: Workspace;
@@ -13,6 +14,12 @@ type InspectorProps = {
 
 const Inspector = (props: InspectorProps) => {
   const [collapsed, setCollapsed] = useState<any>({});
+
+  const menu = (
+    <Menu>
+      <Menu.Item icon="plus" onClick={() => {}} text="New"/>
+    </Menu>
+  );
   const nodes: ITreeNode[] = [
     {
       id: 'asdf',
@@ -54,8 +61,14 @@ const Inspector = (props: InspectorProps) => {
       {props.workspace.project?.views[viewId].name + (viewId === props.workspace.project?.mainView ? ' (main)' : '')}
     </option>)
     : <option>No views available</option>;
+  const onContextMenuHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    ContextMenu.show(menu, { left: e.clientX, top: e.clientY }, () => {
+      // menu was closed; callback optional
+    });
+  }
   return (
-    <>
+    <div onContextMenu={onContextMenuHandler}>
       <HTMLSelect value={props.workspace.selectedViewId || undefined} onChange={e => props.onViewSelectionChange(e.target.value)}>
         {options}
       </HTMLSelect>
@@ -63,7 +76,7 @@ const Inspector = (props: InspectorProps) => {
             onNodeCollapse={onNodeCollapseHandler}
             onNodeExpand={onNodeExpandHandler}/>
 
-    </>
+    </div>
   )
 };
 
